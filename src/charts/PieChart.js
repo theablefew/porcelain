c2691db045e8beff2169a433b03153a5ddd41345
Jason Luebke <jason@theablefew.com>
2014-05-20 12:48:10 -0400

    Remove crossfilter from piechart

--- src/charts/PieChart.js
+++ src/charts/PieChart.js
@@ -3,29 +3,14 @@ function PieChart (element) {
   BaseChart.call(this);
   this.element = element;
 
-  this._renderLabelPaths = function () {
+  this._getCentroid = function (d, r) {
+    return d3.svg.arc()
+      .outerRadius(this.radius+r)
+      .innerRadius(this.radius+r)
+      .centroid(d);
+  };
 
-    var self = this
-      , getCentroid = function (d, r) {
-        return d3.svg.arc()
-          .outerRadius(self.radius+r)
-          .innerRadius(self.radius+r)
-          .centroid(d);
-      }
-
-    this.chart.selectAll('g.pie-slice')
-    .append('path')
-    .attr('class', 'pie-callout')
-    .attr('d', function (d) {
-      // var centroid_outside = getCentroid(d, this.chart.externalLabels()(d)-20)
-      var centroid_outside = getCentroid(d, self.offset-25)
-        , centroid_inside  = getCentroid(d, 0);
-
-      return d3.svg.line()([centroid_inside, centroid_outside]);
-    });
-  }
-
-};
+}
 
 
 Util.extendChart(PieChart, BaseChart);
@@ -33,32 +18,56 @@ Util.extendChart(PieChart, BaseChart);
 
 PieChart.prototype.render = function () {
 
-  var self = this;
+  var self = this
+    , offset_padding = 10;
 
-  // var offsets = options['offsets'] || {}
-  var offsets = {}
-    , g_ndx = crossfilter(this.data)
-    , g_dim = g_ndx.dimension(function(d) {return d.key;})
-    , g_grp = g_dim.group().reduceSum(function(d) {return d.value;});
+  var arc = d3.svg.arc()
+      .outerRadius(this.radius)
+      .innerRadius(this.inner_radius);
 
-  this.chart = dc.pieChart(this.element)
-    .width(this.size.width)
-    .height(this.size.height)
-    .radius(this.radius)
-    .innerRadius(this.inner_radius)
-    .externalLabels(function (d) { return offsets[d.data.key] || self.offset;})
-    .minAngleForLabel(0)
-    .dimension(g_dim)
-    .colors(d3.scale.ordinal().domain(self.theme.domain).range(self.theme.range))
-    .group(g_grp);
+  var arc_label = d3.svg.arc()
+      .outerRadius(this.radius + this.label_offset*2)
+      .innerRadius((this.label_offset > 0) ? this.radius : this.inner_radius);
 
-  this.chart.render();
+  var color = d3.scale.ordinal()
+      .domain(this.theme.domain)
+      .range(this.theme.range);
 
-}
+  var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.value; });
 
+  this.chart = d3.select(this.element).append("svg")
+      .attr("width", this.size.width)
+      .attr("height", this.size.height)
+    .append("g")
+      .attr("transform", "translate(" + this.size.width / 2 + "," + this.size.height / 2 + ")");
 
-PieChart.prototype.afterRender = function () {
-  this._renderLabelPaths();
+  var g = this.chart.selectAll(".pie-slice")
+      .data(pie(this.data))
+    .enter().append("g")
+      .attr("class", "pie-slice");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.key); });
+
+  g.append("text")
+      .attr("transform", function(d) {
+        return "translate(" + arc_label.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.key; });
+
+  g.append('path')
+    .attr('class', 'pie-callout')
+    .attr('d', function (d) {
+      var centroid_outside = self._getCentroid(d, self.label_offset-offset_padding)
+        , centroid_inside  = self._getCentroid(d, offset_padding);
+
+      return d3.svg.line()([centroid_inside, centroid_outside]);
+    });
+
 }
 
 
@@ -96,16 +105,16 @@ PieChart.prototype.defineCapability(
 
 
 PieChart.prototype.defineCapability(
-    'offset', {
+    'label_offset', {
         property: {
-            get        : function ( ) { return this._offset; }
-          , set        : function (_) { this._offset = _; }
+            get        : function ( ) { return this._label_offset; }
+          , set        : function (_) { this._label_offset = _; }
           , enumerable : true
         }
       , descriptor: {
             defined_in  : PieChart
           , description : 'Measure of how far outside chart to render externalLabels labels'
-          , default     : 20
+          , default     : 0
           , required    : false
           , type        : 'int'
         }
