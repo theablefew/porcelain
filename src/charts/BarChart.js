763968ab4d64749db93969a32e459e3d9fa1613a
Jason Luebke <jason@theablefew.com>
2014-05-21 14:52:19 -0400

    Add labels and rotation to barchart

--- src/charts/BarChart.js
+++ src/charts/BarChart.js
@@ -19,6 +19,29 @@ function BarChart (element) {
     .append("g")
       .attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")");
 
+  this._addBarLabels = function () {
+
+    var self = this;
+
+    this.chart.append("g")
+        .attr("class", "labels")
+      .selectAll('.label')
+      .data(this.data)
+      .enter().append("text")
+        .text(function (d) { return d.value; })
+          .attr("x", function(d) { return self.x(d.key)+self.x.rangeBand()/2; })
+          .attr("y", function(d) { return self.y(d.value)-5; })
+          .style('text-anchor', 'middle');
+  }
+
+  this._rotateLabel = function () {
+    this.chart.select('.axis.x').selectAll('g.tick text')
+      .attr('transform', 'rotate('+this.label_rotation+')')
+      .style('text-anchor', 'start');
+  }
+
+
+
 }
 
 
@@ -59,18 +82,8 @@ BarChart.prototype.render = function () {
 
 BarChart.prototype.afterRender = function () {
 
-  var self = this;
-
-  if(this.labels) {
-    this.chart.append("g")
-        .attr("class", "labels")
-      .selectAll('.label')
-      .data(this.data)
-      .enter().append("text")
-        .text(function (d) { return d.value; })
-          .attr("x", function(d) { return self.x(d.key)+self.x.rangeBand()/2; })
-          .attr("y", function(d) { return self.y(d.value)-5; });
-  }
+  if(this.labels) this._addBarLabels();
+  this._rotateLabel();
 
 };
 
@@ -90,6 +103,39 @@ BarChart.prototype.defineCapability(
           , type        : 'boolean'
         }
     });
+
+BarChart.prototype.defineCapability(
+  'focus', {
+      property: {
+          get         : function ( ) { return this._focus; }
+        , set         : function (_) { this._focus = _; }
+        , enumerable  : true
+      }
+    , descriptor: {
+          defined_in  : BarChart
+        , description : 'Highlights data on the chart with an alternate color'
+        , default     : []
+        , required    : false
+        , type        : 'JSON'
+      }
+  });
+
+
+BarChart.prototype.defineCapability(
+  'label_rotation', {
+      property: {
+          get         : function ( ) { return this._label_rotation; }
+        , set         : function (_) { this._label_rotation = _; }
+        , enumerable  : true
+      }
+    , descriptor: {
+          defined_in  : BarChart
+        , description : 'Degrees of which to rotate the labels on the x axis'
+        , default     : 0
+        , required    : false
+        , type        : 'int'
+      }
+  });
 
 
 Porcelain.register('BarChart', BarChart);
