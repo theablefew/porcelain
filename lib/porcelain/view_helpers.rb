cef9d0ee45c122b7dec5db8a8ad6647945fa5552
michaeltomko <mike@tomkobombco.com>
2014-06-11 12:01:28 -0600

    Ruby style guide cleanup of View Helper.

--- lib/porcelain/view_helpers.rb
+++ lib/porcelain/view_helpers.rb
@@ -1,13 +1,9 @@
+# lib/porcelain/view_helpers.rb
 module Porcelain
+  # View helper to render Porcelain charts.
   module ViewHelpers
-    def render_porcelain_chart(chart_type,capabilities)
-      content_tag(:div, 
-                  "", 
-                  :class => "porcelain-chartable", 
-                  :data => {:capabilities => capabilities,
-                    :chart_type => chart_type
-                  }
-                 )    
+    def render_porcelain_chart(chart_type, capabilities)
+      content_tag(:div, '', class: 'porcelain-chartable', data: { capabilities: capabilities, chart_type: chart_type })
     end
   end
 end
